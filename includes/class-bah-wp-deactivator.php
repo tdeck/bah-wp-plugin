9d72d4e4f09631a0d24dab0c4120f3aca3d5cff1
Troy H. Deck <troy.deque@gmail.com>
2018-07-03 23:18:48 -0700

    Add plugin boilerplate from wppb.me

---
+++ includes/class-bah-wp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/tdeck
+ * @since      1.0.0
+ *
+ * @package    Bah_Wp
+ * @subpackage Bah_Wp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Bah_Wp
+ * @subpackage Bah_Wp/includes
+ * @author     Troy Deck <troy.deque@gmail.com>
+ */
+class Bah_Wp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
